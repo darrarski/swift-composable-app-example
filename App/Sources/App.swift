827f8483cdf5a680e3c9a126a2cca0de1e218513
Dariusz Rybicki <darrarski@gmail.com>
2021-01-31 18:45:50 +0100

    Add TabsView to App

--- App/Sources/App.swift
+++ App/Sources/App.swift
@@ -1,12 +1,26 @@
+import Color
 import Common
+import ComposableArchitecture
+import Preview
+import Shape
 import SwiftUI
+import Tabs
 
 @main
 struct App: SwiftUI.App {
   var body: some Scene {
     WindowGroup {
       if !isRunningTests {
-        Text("Hello, World!")
+        TabsView(store: Store(
+          initialState: TabsState(
+            selectedTab: .preview,
+            color: ColorState(rgb: RGBColor(0.5, 0.5, 0.5)),
+            shape: ShapeState(type: .square),
+            preview: PreviewState()
+          ),
+          reducer: tabsReducer,
+          environment: ()
+        ))
       }
     }
   }

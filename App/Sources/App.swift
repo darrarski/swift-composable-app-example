4d5662714924fdf3496e7bca7fe75063d089f617
Dariusz Rybicki <darrarski@gmail.com>
2021-01-31 12:35:49 +0100

    Add Xcode project with iOS app and unit tests targets

---
+++ App/Sources/App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct App: SwiftUI.App {
+  var body: some Scene {
+    WindowGroup {
+      Text("Hello, world!")
+        .padding()
+    }
+  }
+}
---
+++ App/Tests/AppTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import App
+
+final class AppTests: XCTestCase {
+  func testExample() throws {
+    XCTAssert(true)
+  }
+}
